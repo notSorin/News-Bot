b1a25673a8e768183d61f33f3deb5c1df24e8ff2
Sorin <sorindra@ucm.es>
2021-05-04 17:44:39 +0200

    First commit.

---
+++ src/chatbot/TelegramChatbot.java
@@ -0,0 +1,55 @@
+package chatbot;
+
+import java.util.Date;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TelegramChatbot extends TelegramLongPollingBot
+{
+	private final String API_TOKEN;
+	
+	public TelegramChatbot(String apiToken)
+	{
+		API_TOKEN = apiToken;
+	}
+	
+	@Override
+	public void onUpdateReceived(Update update)
+	{
+		//We check if the update has a message and the message has text.
+	    if(update.hasMessage() && update.getMessage().hasText())
+	    {
+	    	//Create a SendMessage object with mandatory fields.
+	        SendMessage message = new SendMessage();
+	     
+	        message.setChatId(update.getMessage().getChatId().toString());
+	        //message.setText(_chat.multisentenceRespond(update.getMessage().getText()));
+	        message.setText((new Date().toString()));
+
+	        try
+	        {
+	        	//Call method to send the message.
+	            execute(message);
+	        }
+	        catch(TelegramApiException e)
+	        {
+	            e.printStackTrace();
+	        }
+	    }
+	}
+
+	@Override
+	public String getBotUsername()
+	{
+		return "Whatian";
+	}
+
+	@Override
+	public String getBotToken()
+	{
+		return API_TOKEN;
+	}
+}
---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import chatbot.TelegramChatbot;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		if(args.length == 1)
+		{
+			try
+			{
+				TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+				
+				botsApi.registerBot(new TelegramChatbot(args[0]));
+			}
+			catch(TelegramApiException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			System.err.println("Must provide the bot token as the first argument to the program.");
+		}
+	}
+}
