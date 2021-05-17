ba4e008e7f38b4683b0f53735c6aba405c0b0b58
Sorin <sorindra@ucm.es>
2021-05-17 15:25:42 +0200

    Added ClusteringAgent which will be used for clustering news articles.

---
+++ src/agents/ClusteringAgent.java
@@ -0,0 +1,12 @@
+package agents;
+
+import jade.core.Agent;
+
+/**
+ * Agent used for clustering news articles.
+ * */
+public class ClusteringAgent extends Agent
+{
+	private static final long serialVersionUID = 1L;
+
+}
