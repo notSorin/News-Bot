7a598ddac555d5a1766e40b38cfb8a70964cf25a
Sorin <sorindra@ucm.es>
2021-05-17 16:36:21 +0200

    Created the behavior for the InformationExtractorAgent.

---
+++ src/agents/InformationExtractorBehaviour.java
@@ -0,0 +1,14 @@
+package agents;
+
+import jade.core.behaviours.CyclicBehaviour;
+
+public class InformationExtractorBehaviour extends CyclicBehaviour
+{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void action()
+	{
+		
+	}
+}
