969099ea45815a740187f03e7ec950664e2e9315
Sorin <sorindra@ucm.es>
2021-05-17 16:39:38 +0200

    Added the behavior class for ConversationAgent.

---
+++ src/agents/ConversationAgentBehavior.java
@@ -0,0 +1,14 @@
+package agents;
+
+import jade.core.behaviours.CyclicBehaviour;
+
+public class ConversationAgentBehavior extends CyclicBehaviour
+{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void action()
+	{
+		
+	}
+}
