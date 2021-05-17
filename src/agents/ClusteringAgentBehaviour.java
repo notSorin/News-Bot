1b1c1ac954570f4fafefae19128f2e25a6b9374c
Sorin <sorindra@ucm.es>
2021-05-17 19:17:21 +0200

    Created ClusteringAgentBehaviour for ClusteringAgent.

---
+++ src/agents/ClusteringAgentBehaviour.java
@@ -0,0 +1,20 @@
+package agents;
+
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+
+public class ClusteringAgentBehaviour extends CyclicBehaviour
+{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void action()
+	{
+		ACLMessage message = getAgent().receive();
+		
+		if(message != null)
+		{
+			//TODO
+		}
+	}
+}
