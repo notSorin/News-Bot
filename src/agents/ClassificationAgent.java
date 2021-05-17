7374f0c2b67099346936c8a0c8dd9c117b5af8ca
Sorin <sorindra@ucm.es>
2021-05-17 15:21:58 +0200

    Added ClassificationAgent which will be used for classifying news articles to a specific category.

---
+++ src/agents/ClassificationAgent.java
@@ -0,0 +1,12 @@
+package agents;
+
+import jade.core.Agent;
+
+/**
+ * Agent used for classifying a news article to a specific category.
+ * */
+public class ClassificationAgent extends Agent
+{
+	private static final long serialVersionUID = 1L;
+
+}
