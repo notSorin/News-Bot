8a91c79095d895b75e839053db445523a04350a2
Sorin <sorindra@ucm.es>
2021-05-17 15:18:33 +0200

    Created InformationExtractorAgent which will be used to retrieve relevant information from a news article.

---
+++ src/agents/InformationExtractorAgent.java
@@ -0,0 +1,12 @@
+package agents;
+
+import jade.core.Agent;
+
+/**
+ * Agent used for retrieving relevant information from a news article.
+ * */
+public class InformationExtractorAgent extends Agent
+{
+	private static final long serialVersionUID = 1L;
+
+}
