e6c3436da1d360ab68d4e7330a377b2349431b4a
Sorin <sorindra@ucm.es>
2021-05-27 15:15:45 +0200

    Added the TopicModel class used for topic inference.

---
+++ src/agents/clustering/TopicModel.java
@@ -0,0 +1,72 @@
+package agents.clustering;
+
+import java.io.File;
+import cc.mallet.topics.ParallelTopicModel;
+import cc.mallet.types.Instance;
+import cc.mallet.types.InstanceList;
+
+class TopicModel
+{
+	private ParallelTopicModel _topicModel;
+	private InstanceList _sportInstance;
+	
+	TopicModel()
+	{
+		try
+		{
+			//Load the model from file.
+			_topicModel = ParallelTopicModel.read(new File("./resources/mallet/model"));
+			
+			//Load the sport instance list from file.
+			_sportInstance = InstanceList.load(new File("./resources/mallet/sport.mallet"));
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	String inferTopics(String article)
+	{
+		String inferredTopics = null;
+		
+		//Create a new instance list using the pipe from the sport instance.
+		final InstanceList newInstances = new InstanceList(_sportInstance.getPipe());
+		
+		//Add the article's text as an Instance to the new instance list.
+		newInstances.addThruPipe(new Instance(article, "user article", null, null));
+		
+		//Get the sampled distribution for the new instance.
+		final double [] distributions = _topicModel.getInferencer().getSampledDistribution(newInstances.get(0), 10, 1, 5);
+		double maxDistribution = distributions[0];
+		int maxDistributionPosition = 0;
+		
+		//Find the greatest distribution value.
+		for(int i = 0; i < distributions.length; i++)
+		{
+			if(distributions[i] > maxDistribution)
+			{
+				maxDistribution = distributions[i];
+				maxDistributionPosition = i;
+			}
+		}
+		
+		//Get the top 20 words from the topic with the greatest distribution value.
+		Object[][] topWords = _topicModel.getTopWords(20);
+		final int totalTopWords = topWords[maxDistributionPosition].length;
+		
+		if(totalTopWords > 0)
+		{
+			inferredTopics = "These are the topics of your article, matching at " + maxDistribution + " %...\n";
+			final int toLoop = totalTopWords > 20 ? 20 : totalTopWords;
+			
+			//Only return the words from the topic with the greatest distribution value (maxDistributionPosition).
+			for(int i = 0; i < toLoop; i++)
+			{
+				inferredTopics += topWords[maxDistributionPosition][i] + " ";
+			}
+		}
+		
+		return inferredTopics;
+	}
+}
