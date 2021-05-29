ea5311030b27b69a5c12a47a76f2710168c25dfd
Sorin <sorindra@ucm.es>
2021-05-29 14:25:34 +0200

    Added comments.

--- src/agents/NewsBotAgent.java
+++ src/agents/NewsBotAgent.java
@@ -9,23 +9,36 @@ import jade.core.AID;
 import jade.core.Agent;
 import jade.lang.acl.ACLMessage;
 
+/**
+ * An abstract Agent extended with some default methods, extended by other Agents.
+ * @author Sorin
+ *
+ */
 public abstract class NewsBotAgent extends Agent
 {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Sends a response to the ConversationAgent.
+	 * @param mv Indicates to what the Agent is responding.
+	 * @param response The actual data of the response.
+	 */
 	public void respondToRequest(MessageValue mv, Object response)
 	{
+		//Create the content of the message.
 		HashMap<MessageKey, Object> aclContent = new HashMap<MessageKey, Object>();
 
 		aclContent.put(MessageKey.INTENT, mv);
 		aclContent.put(MessageKey.INTENT_DATA, response);
 
+		//Create the ACL message.
 		ACLMessage aclMessage = new ACLMessage(ACLMessage.INFORM);
 
 		aclMessage.addReceiver(new AID(ConversationAgent.class.getSimpleName(), AID.ISLOCALNAME));
 		
 		try
 		{
+			//Set the content of the message and send it.
 			aclMessage.setContentObject(aclContent);
 			send(aclMessage);
 		}
