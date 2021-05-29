23cff99e2efd9c0e2feb6ecf39778111626461e4
Sorin <sorindra@ucm.es>
2021-05-29 20:02:01 +0200

    Implemented performExtraction().

--- src/agents/informationextractor/InformationExtractorBehavior.java
+++ src/agents/informationextractor/InformationExtractorBehavior.java
@@ -3,6 +3,7 @@ package agents.informationextractor;
 import java.util.HashMap;
 import agents.AgentsEnums.MessageKey;
 import agents.AgentsEnums.MessageValue;
+import gate.AnnotationSet;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 
@@ -15,6 +16,15 @@ class InformationExtractorBehavior extends CyclicBehaviour
 {
 	private static final long serialVersionUID = 1L;
 	private static final String EXTRACT_EXCEPTION = "It seems that I was not able to perform this action.";
+	private static final String EXTRACTION_FAILURE = "I could not extract any information from the article.";
+	private static final String EXTRACTION_SUCCESS = "Information extraction completed.\nWhat information would you like to know about it?";
+	
+	private final GATEHandle GATE_HANDLE;
+	
+	public InformationExtractorBehavior()
+	{
+		GATE_HANDLE = new GATEHandle();
+	}
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -62,7 +72,14 @@ class InformationExtractorBehavior extends CyclicBehaviour
 	 */
 	private String performExtraction(String article)
 	{
-		//TODO
-		return null;
+		String ret = EXTRACTION_FAILURE;
+		AnnotationSet annotations = GATE_HANDLE.getAnnotations(article);
+		
+		if(annotations != null)
+		{
+			ret = EXTRACTION_SUCCESS;
+		}
+		
+		return ret;
 	}
 }
